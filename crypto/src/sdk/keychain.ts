340c3dc7455053e8e5df996852e47cf9912e8bbd
pycckuu <is.markelov@gmail.com>
2022-10-14 15:39:25 +0200

    root: refactor moving services/keychain to crypto/sdk/keychain
    Without this patch, the keychain service is not able to be used by the
    contracts workspace. This patch moves the keychain service to the
    crypto/sdk/keychain. Then, the SDK keychain functions are used to derive
    the keys in tests. Also, the tx error handling is moved to the crypto
    workspace.
    
    Issue-URL: https://app.clickup.com/t/3kqrj3c

---
+++ crypto/src/sdk/keychain.ts
@@ -0,0 +1,76 @@
+import {
+    deriveKeypairFromSignature,
+    deriveChildPrivKeyFromRootPrivKey,
+    deriveKeypairFromSeed,
+    deriveChildPubKeyFromRootPubKey,
+    isChildPubKeyValid,
+} from '../keychain';
+import {IKeypair, PrivateKey} from '../types/keypair';
+import {poseidon} from 'circomlibjs';
+import {Signer} from 'ethers';
+
+import {parseTxErrorMessage} from '../errors';
+
+// generateSpendingChildKeypair generates child spending keypair (s', S')
+// using root spending private key and random scalar r as input.
+export function generateSpendingChildKeypair(
+    rootSpendingPrivKey: PrivateKey,
+    r: bigint,
+): IKeypair {
+    const spendingChildPrivKey = deriveChildPrivKeyFromRootPrivKey(
+        rootSpendingPrivKey,
+        r,
+    );
+    return deriveKeypairFromSeed(spendingChildPrivKey);
+}
+
+export async function deriveRootKeypairs(
+    signer: Signer,
+): Promise<IKeypair[] | Error> {
+    const derivationMessage = `Greetings from Panther Protocol!
+
+Sign this message in order to obtain the keys to your Panther wallet.
+
+This signature will not cost you any fees.
+
+Keypair version: 1`;
+    let signature: string;
+    try {
+        signature = await signer.signMessage(derivationMessage);
+    } catch (error) {
+        return new Error(parseTxErrorMessage(error));
+    }
+
+    const hashedSignature = poseidon([signature]);
+    return [
+        deriveKeypairFromSignature(signature),
+        deriveKeypairFromSeed(hashedSignature),
+    ];
+}
+
+export function deriveSpendingChildKeypair(
+    rootSpendingKeypair: IKeypair,
+    randomSecret: bigint,
+): [IKeypair, boolean] {
+    const childSpendingPrivateKey = deriveChildPrivKeyFromRootPrivKey(
+        rootSpendingKeypair.privateKey,
+        randomSecret,
+    );
+
+    const spendingChildPubKey = deriveChildPubKeyFromRootPubKey(
+        rootSpendingKeypair.publicKey,
+        randomSecret,
+    );
+
+    console.debug('derived spenderChildPubKey:', spendingChildPubKey);
+    const isValid = isChildPubKeyValid(
+        spendingChildPubKey,
+        rootSpendingKeypair,
+        randomSecret,
+    );
+
+    return [
+        {privateKey: childSpendingPrivateKey, publicKey: spendingChildPubKey},
+        isValid,
+    ];
+}
