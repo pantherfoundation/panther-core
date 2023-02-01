496bcd018c031a2387d1efabd79422df547d8981
SushmaVaradaiah-PP <sushma@pantherprotocol.io>
2023-02-01 12:00:29 +0530

    test(circuits): test cases for nullifierhasher circuit
    Without this patch there won't be any testing for this circuit.
    
    This is a problem because without test cases we might miss some
    potential bugs in the circuit.
    
    This patch solves the problem by testing the circuit through test cases.
    
    Issue-URL: https://app.clickup.com/t/85zrgkazu

---
+++ circuits/test/NullifierHasher.test.ts
@@ -0,0 +1,90 @@
+import * as path from 'path';
+import {expect} from 'chai';
+
+import cicom_wasm_tester from 'circom_tester';
+const wasm_tester = cicom_wasm_tester.wasm;
+
+import {babyjub} from 'circomlibjs';
+const F = babyjub.F;
+
+import {getOptions} from './helpers/circomTester';
+import {getRandomInt} from './helpers/utility';
+import poseidon from 'circomlibjs/src/poseidon';
+
+describe('NullifierHasher circuit', async function (this: any) {
+    interface Input {
+        spendPrivKey: bigint;
+        leaf: bigint;
+    }
+
+    let signalInput: Input;
+    let nullifierHasher: any;
+
+    this.timeout(10000000);
+
+    before(async function () {
+        const opts = getOptions();
+        const input = path.join(
+            opts.basedir,
+            './test/circuits/nullifierHasher.circom',
+        );
+        nullifierHasher = await wasm_tester(input, opts);
+    });
+
+    beforeEach(async function () {
+        signalInput = {
+            spendPrivKey: BigInt(0),
+            leaf: BigInt(0),
+        };
+    });
+
+    describe('Valid input signals', async function () {
+        it('should compute valid nullifier when input signal values are 0', async () => {
+            const w = await nullifierHasher.calculateWitness(signalInput, true);
+
+            const out = poseidon([BigInt(0), BigInt(0)]);
+
+            await nullifierHasher.assertOut(w, {out: out});
+        });
+
+        it('should compute valid nullifier when input signal values are random', async () => {
+            let value = BigInt(getRandomInt(0, 123456789));
+
+            signalInput = {
+                spendPrivKey: F.e(value),
+                leaf: F.e(value),
+            };
+
+            const w = await nullifierHasher.calculateWitness(signalInput, true);
+
+            const out = poseidon([value, value]);
+
+            await nullifierHasher.assertOut(w, {out: out});
+        });
+    });
+
+    describe('Invalid input signals', async function () {
+        it('should fail to compute valid nullifier when input signal values are above signal range', async () => {
+            let value = BigInt(0);
+            for (let i = 0; i <= 256; i++) {
+                value += BigInt(Math.pow(2, i));
+            }
+            signalInput = {
+                spendPrivKey: value,
+                leaf: value,
+            };
+
+            const w = await nullifierHasher.calculateWitness(signalInput, true);
+
+            const out = poseidon([value, value]);
+
+            try {
+                await nullifierHasher.assertOut(w, {out: out});
+                console.log(`Error occured for input signal ${value}`);
+                throw new Error(`This code should never be reached!`);
+            } catch (err) {
+                expect(err).to.be.instanceOf(Error);
+            }
+        });
+    });
+});
---
+++ circuits/test/circuits/nullifierHasher.circom
@@ -0,0 +1,4 @@
+pragma circom 2.0.0;
+include "../../circuits/templates/nullifierHasher.circom";
+
+component main = NullifierHasher();
