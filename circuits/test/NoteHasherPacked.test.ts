2ac5fc54130c1fed91f6ba8c25ffdfb01cee1f24
SushmaVaradaiah-PP <sushma@pantherprotocol.io>
2023-02-01 15:24:36 +0530

    test(circuits): test cases for noteHasher and notehasherpacked circuits
    Without this patch there won't be any testing for these circuits.
    
    This is a problem because without test cases we might miss some
    potential bugs in these circuits.
    
    This patch solves the problem by testing the circuits via test cases.
    
    Issue-URL: https://app.clickup.com/t/85zrgkazu

--- circuits/test/NoteHasherPacked.test.ts
+++ circuits/test/NoteHasherPacked.test.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import {expect} from 'chai';
 
 import cicom_wasm_tester from 'circom_tester';
 const wasm_tester = cicom_wasm_tester.wasm;
@@ -7,17 +8,23 @@ import {babyjub} from 'circomlibjs';
 const F = babyjub.F;
 
 import {getOptions} from './helpers/circomTester';
-
+import {getRandomInt} from './helpers/utility';
 import {poseidon} from 'circomlibjs';
 
-const getRandomInt = (min: number, max: number) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-};
-
 describe('NoteHasherPacked circuit', async function (this: any) {
-    let circuit: any;
+    interface Input {
+        spendPk: bigint[];
+        amount: bigint;
+        token: bigint;
+        createTime: bigint;
+    }
+
+    let signalInput: Input;
+    let noteHasherPacked: any;
+    let pk: bigint[] = [BigInt(1), BigInt(2)];
+    let amount: bigint;
+    let token: bigint;
+    let createTime: bigint;
 
     this.timeout(10000000);
 
@@ -27,47 +34,144 @@ describe('NoteHasherPacked circuit', async function (this: any) {
             opts.basedir,
             './test/circuits/noteHasherPacked.circom',
         );
-        circuit = await wasm_tester(input, opts);
+        noteHasherPacked = await wasm_tester(input, opts);
     });
 
-    it('Should compute valid commitment for ZERO', async function () {
-        let value = BigInt(0);
-        const input = {
-            spendPk: [F.e(value), F.e(value)],
-            amount: F.e(value),
-            token: F.e(value),
-            createTime: F.e(value),
-        };
-
-        // poseidon(pk[0],pk[1],packed(amount,token,createTime))
-        const out = poseidon([value, value, value]);
-
-        const w = await circuit.calculateWitness(input, true);
-
-        await circuit.assertOut(w, {out: out});
-    });
-
-    it('Should compute valid commitment for random input', async function () {
-        const pk = [
-            BigInt(getRandomInt(0, 123456789)),
-            BigInt(getRandomInt(0, 123456789)),
-        ];
-        const amount = BigInt(getRandomInt(0, 123456789));
-        const token = BigInt(getRandomInt(0, 123456789));
-        const createTime = BigInt(Math.round(Date.now() / 1000));
-        const input = {
+    beforeEach(async function () {
+        pk = [BigInt(0), BigInt(0)];
+        amount = BigInt(0);
+        token = BigInt(0);
+        createTime = BigInt(0);
+        signalInput = {
             spendPk: [F.e(pk[0]), F.e(pk[1])],
             amount: F.e(amount),
             token: F.e(token),
             createTime: F.e(createTime),
         };
-        const packed =
-            (amount << BigInt(192)) | (token << BigInt(32)) | createTime;
-        // poseidon(pk[0],pk[1],packed(amount,token,createTime))
-        const out = poseidon([pk[0], pk[1], packed]);
+    });
 
-        const w = await circuit.calculateWitness(input, true);
+    describe('Valid input signals', async function () {
+        it('should compute valid poseidon hash when input signal values are 0', async function () {
+            const w = await noteHasherPacked.calculateWitness(
+                signalInput,
+                true,
+            );
 
-        await circuit.assertOut(w, {out: out});
+            const packed =
+                (amount << BigInt(192)) | (token << BigInt(32)) | createTime;
+
+            const out = poseidon([pk[0], pk[1], packed]);
+
+            await noteHasherPacked.assertOut(w, {out: out});
+        });
+
+        it('should computes valid poseidon hash when input signal values are random', async function () {
+            const pk = [
+                BigInt(getRandomInt(0, 123456789)),
+                BigInt(getRandomInt(0, 123456789)),
+            ];
+            amount = BigInt(getRandomInt(0, 123456789));
+            token = BigInt(getRandomInt(0, 123456789));
+            createTime = BigInt(Math.round(Date.now() / 1000));
+            signalInput = {
+                spendPk: [F.e(pk[0]), F.e(pk[1])],
+                amount: F.e(amount),
+                token: F.e(token),
+                createTime: F.e(createTime),
+            };
+
+            const w = await noteHasherPacked.calculateWitness(
+                signalInput,
+                true,
+            );
+
+            const packed =
+                (amount << BigInt(192)) | (token << BigInt(32)) | createTime;
+            // poseidon(pk[0],pk[1],packed(amount,token,createTime))
+            const out = poseidon([pk[0], pk[1], packed]);
+
+            await noteHasherPacked.assertOut(w, {out: out});
+        });
+    });
+
+    describe('Invalid input signals', async function () {
+        it('should fail to compute valid poseidon hash when signal values are not packed properly', async function () {
+            pk = [
+                BigInt(getRandomInt(0, 123456789)),
+                BigInt(getRandomInt(0, 123456789)),
+            ];
+            amount = BigInt(getRandomInt(0, 123456789));
+            token = BigInt(getRandomInt(0, 123456789));
+            createTime = BigInt(Math.round(Date.now() / 1000));
+            signalInput = {
+                spendPk: [F.e(pk[0]), F.e(pk[1])],
+                amount: F.e(amount),
+                token: F.e(token),
+                createTime: F.e(createTime),
+            };
+
+            const w = await noteHasherPacked.calculateWitness(
+                signalInput,
+                true,
+            );
+
+            const packed = amount | token | createTime;
+            const out = poseidon([pk[0], pk[1], packed]);
+
+            try {
+                await noteHasherPacked.assertOut(w, {out: out});
+                console.log(`Error occured for input signal ${signalInput}`);
+                throw new Error(`This code should never be reached!`);
+            } catch (err) {
+                expect(err).to.be.instanceOf(Error);
+            }
+        });
+
+        it('should fail to compute valid poseidon hash when signal values are above signal range', async function () {
+            let spendPkValue = BigInt(0);
+            for (let i = 0; i <= 256; i++) {
+                spendPkValue += BigInt(Math.pow(2, i));
+            }
+            pk = [spendPkValue, spendPkValue];
+
+            amount = BigInt(0);
+            for (let i = 0; i <= 63; i++) {
+                amount += BigInt(Math.pow(2, i));
+            }
+
+            token = BigInt(0);
+            for (let i = 0; i <= 159; i++) {
+                token += BigInt(Math.pow(2, i));
+            }
+
+            createTime = BigInt(0);
+            for (let i = 0; i <= 31; i++) {
+                createTime += BigInt(Math.pow(2, i));
+            }
+
+            signalInput = {
+                spendPk: [pk[0], pk[1]],
+                amount: amount,
+                token: token,
+                createTime: createTime,
+            };
+
+            const w = await noteHasherPacked.calculateWitness(
+                signalInput,
+                true,
+            );
+
+            const packed =
+                (amount << BigInt(192)) | (token << BigInt(32)) | createTime;
+            const out = poseidon([pk[0], pk[1], packed]);
+
+            try {
+                await noteHasherPacked.assertOut(w, {out: out});
+                console.log(`Error occured for input signal ${spendPkValue}`);
+                throw new Error(`This code should never be reached!`);
+            } catch (err) {
+                expect(err).to.be.instanceOf(Error);
+            }
+        });
     });
 });
---
+++ circuits/test/circuits/noteHasher.circom
@@ -0,0 +1,4 @@
+pragma circom 2.0.0;
+include "../../circuits/templates/noteHasher.circom";
+
+component main = NoteHasher();
