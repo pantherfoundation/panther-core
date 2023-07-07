d77163652c9b636cd34a91609babcd4e4b6672ee
Behzad <whoisbehzad@gmail.com>
2023-07-07 18:58:02 +0330

    feat(contracts): modify simulation adding utxo
    Without this patch, the reward is defined as a parameter. It means
    anyone who adds UTXOs is able to put any desired value as reward.
    
    This is a problem because at testing stage, the miners are supposed to
    add UTXOs hence they can define the reward value.
    since the reward value will be exchangeable for real ZKP, then the
    miner may define huge rewards in order to be able to exchange these
    test rewards (ZKP on mumbai) with real ZKP (ZKP on polygon).
    
    This patch solves the problem by modifying the
    `simulateAddUtxosToBusQueue` to calculate `utxos[]` and `reward`
    internally without getting any parameter from external.
    The reward is calculated based on a formula defined in the contract.
    
    Issue-URL: https://app.clickup.com/t/865cpk7mw

--- contracts/contracts/protocol/pantherForest/mocks/MockBusTree.sol
+++ contracts/contracts/protocol/pantherForest/mocks/MockBusTree.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 // SPDX-FileCopyrightText: Copyright 2023 Panther Ventures Limited Gibraltar
+// solhint-disable var-name-mixedcase
 pragma solidity ^0.8.16;
 
 import "../busTree/BusTree.sol";
@@ -8,11 +9,46 @@ import { FIELD_SIZE } from "../../crypto/SnarkConstants.sol";
 import { DEAD_CODE_ADDRESS } from "../../../common/Constants.sol";
 
 contract MockBusTree is BusTree {
+    // The contract is supposed to run behind a proxy DELEGATECALLing it.
+    // On upgrades, adjust `__gap` to match changes of the storage layout.
+    // slither-disable-next-line shadowing-state unused-state
+    uint256[50] private __gap;
+
+    // avg utxos which can be added per minute
+    uint256 public immutable AVG_UTXOS_PER_MINUTE;
+
+    // max number of utxos to be added
+    uint256 public immutable UTXO_LIMIT;
+
+    // base reward per each utxo
+    uint256 public immutable BASE_REWARD_PER_UTXO;
+
+    // timestamp of deployment
+    uint256 public immutable START_TIME;
+
+    // keeps track of number of the added utxos
+    uint256 public utxoCounter;
+
     event MinerRewarded(address miner, uint256 reward);
 
-    constructor(address _verifier, uint160 _circuitId)
-        BusTree(_verifier, _circuitId)
-    {} // solhint-disable-line no-empty-blocks
+    constructor(
+        address _verifier,
+        uint160 _circuitId,
+        uint256 _avgUtxosPerMinute,
+        uint256 _utxoLimit,
+        uint256 _baseRewardPerUtxo
+    ) BusTree(_verifier, _circuitId) {
+        require(
+            _avgUtxosPerMinute > 0 && _utxoLimit > 0 && _baseRewardPerUtxo > 0,
+            "init: zero value"
+        );
+
+        AVG_UTXOS_PER_MINUTE = _avgUtxosPerMinute;
+        UTXO_LIMIT = _utxoLimit;
+        BASE_REWARD_PER_UTXO = _baseRewardPerUtxo;
+
+        START_TIME = block.timestamp;
+    }
 
     function rewardMiner(address miner, uint256 reward) internal override {
         emit MinerRewarded(miner, reward);
@@ -31,7 +67,56 @@ contract MockBusTree is BusTree {
         return PoseidonT3.poseidon([left, right]);
     }
 
-    function simulateAddUtxosToBusQueue(bytes32[] memory utxos, uint96 reward)
+    function getAllowedUtxosAt(uint256 _timestamp, uint256 _utxoCounter)
+        public
+        view
+        returns (uint256 allowedUtxos)
+    {
+        if (_timestamp < START_TIME) return 0;
+
+        allowedUtxos =
+            ((_timestamp - START_TIME) / 60 seconds) *
+            AVG_UTXOS_PER_MINUTE -
+            _utxoCounter;
+    }
+
+    function simulateAddUtxosToBusQueue() external {
+        uint256 _counter = utxoCounter;
+
+        // generating the first utxo
+        uint256 utxo = uint256(keccak256(abi.encode(_counter))) % FIELD_SIZE;
+
+        // Generating the utxos length between 1 - 5
+        uint256 length = (utxo & 3) + 1;
+
+        if (_counter + length > getAllowedUtxosAt(block.timestamp, _counter))
+            return;
+
+        bytes32[] memory utxos;
+
+        // adding the first commitment
+        utxos[0] = bytes32(utxo);
+        _counter++;
+
+        // adding the rest of commitment
+        for (uint256 i = 1; i < length; ) {
+            utxos[i] = bytes32(
+                uint256(keccak256(abi.encode(_counter))) % FIELD_SIZE
+            );
+
+            unchecked {
+                i++;
+                _counter++;
+            }
+        }
+
+        utxoCounter = _counter;
+        uint256 reward = BASE_REWARD_PER_UTXO * length;
+
+        addUtxosToBusQueue(utxos, uint96(reward));
+    }
+
+    function _simulateAddUtxosToBusQueue(bytes32[] memory utxos, uint96 reward)
         external
     {
         addUtxosToBusQueue(utxos, reward);
@@ -51,6 +136,7 @@ contract MockBusTree is BusTree {
             uint96 reward
         )
     {
+        // solhint-disable-next-line avoid-tx-origin
         require(tx.origin == DEAD_CODE_ADDRESS, "Only allowed in forked env");
         return setBusQueueAsProcessed(queueId);
     }
