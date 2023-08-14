6ccbd6394a74ac8cc1614bc566fd06c03cd197a5
Behzad <whoisbehzad@gmail.com>
2023-08-14 11:37:23 +0400

    fix(contracts): add init err msg of bus tree

--- contracts/contracts/protocol/errMsgs/PantherBusTreeErrMsgs.sol
+++ contracts/contracts/protocol/errMsgs/PantherBusTreeErrMsgs.sol
@@ -2,5 +2,6 @@
 // SPDX-FileCopyrightText: Copyright 2021-22 Panther Ventures Limited Gibraltar
 pragma solidity ^0.8.16;
 
+string constant ERR_INIT = "PBT:E1";
 string constant ERR_UNAUTHORIZED = "PBT:E2";
 string constant ERR_ZERO_REWARD_PARAMS = "PBT:E3";
