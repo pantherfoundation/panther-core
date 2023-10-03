2afe2ee0b0cc35baf8a7fd2ee5b07eb1585e7d41
Behzad <whoisbehzad@gmail.com>
2023-10-03 09:54:11 +0400

    chore(contracts): relocate contracts
    Without this patch, the contracts which are supposed to handle protocol
    rewards are placed in the `contracts/rewards` directory.
    
    This patch solves the problem by moving them into
    the `contracts/protocol` directory.

--- contracts/contracts/rewards/mocks/MockGranter.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-// SPDX-FileCopyrightText: Copyright 2023 Panther Ventures Limited Gibraltar
-pragma solidity 0.8.16;
-
-contract MockGranter {
-    function grant(bytes4 grantType, address receiver) external {} // solhint-disable-line no-empty-blocks
-}
