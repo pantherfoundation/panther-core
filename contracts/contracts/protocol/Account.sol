317a74e4d3d29b200ea0f778915878d02c9fe65d
bitnician <bitnician@gmail.com>
2024-02-06 15:38:20 +0400

    feat(contracts): add new empty contract
    This patch adds empty solidity files to recognize the new
    contracts that are supposed to be developed in the future.

---
+++ contracts/contracts/protocol/Account.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2023 Panther Ventures Limited Gibraltar
+pragma solidity 0.8.16;
+
+// solhint-disable-next-line no-empty-blocks
+contract Account {
+
+}
