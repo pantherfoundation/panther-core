7f3876af8b2b8492b6e0f2b00fd8f84f03ff6ac3
bitnician <bitnician@gmail.com>
2024-02-13 12:51:27 +0400

    docs(contracts): pay master doc

--- contracts/contracts/protocol/v1/PayMaster.sol
+++ contracts/contracts/protocol/v1/PayMaster.sol
@@ -2,6 +2,19 @@
 // SPDX-FileCopyrightText: Copyright 2024 Panther Ventures Limited Gibraltar
 pragma solidity ^0.8.19;
 
+/**
+ * @title PayMaster
+ * @author Pantherprotocol Contributors
+ * @dev The paymaster assumes the role of the sponsor for the Panther smart account.
+ * Through the allocation of staked funds in the EntryPoint, this contract has been granted
+ * the capability to facilitate transaction payments.
+ * The paymaster knows in advance the requisite network fee that gonna pay by the user in
+ * ZKP tokens and proactively computes the exchange rate from ZKP tokens to Ethereum using the
+ * Uniswap Time-Weighted Average Price (TWAP).
+ * Upon verification of sufficient funds, transactions proceed successfully, culminating in
+ * the settlement of the user's debt to the paymaster in Ethereum as an integral component of
+ * the transaction process.
+ */
 // solhint-disable-next-line no-empty-blocks
 contract PayMaster {
 
