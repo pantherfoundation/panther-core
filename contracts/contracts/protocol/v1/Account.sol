10e01478306d6dadf2e1d20c85f8f22729021ab8
bitnician <bitnician@gmail.com>
2024-02-13 12:50:19 +0400

    docs(contracts): account doc

--- contracts/contracts/protocol/v1/Account.sol
+++ contracts/contracts/protocol/v1/Account.sol
@@ -2,6 +2,13 @@
 // SPDX-FileCopyrightText: Copyright 2024 Panther Ventures Limited Gibraltar
 pragma solidity ^0.8.19;
 
+/**
+ * @title Account
+ * @author Pantherprotocol Contributors
+ * @dev Singleton account contract for all of the Panther users. Each tx to the protocol
+ * is supposed to have a unique nonce key which must be increased to 1 after execution of
+ * the tx.
+ */
 // solhint-disable-next-line no-empty-blocks
 contract Account {
 
