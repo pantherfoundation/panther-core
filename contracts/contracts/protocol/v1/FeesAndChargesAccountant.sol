6396bda879cf393269a01262a296d7762150667e
bitnician <bitnician@gmail.com>
2024-02-13 12:50:53 +0400

    docs(contracts): fee accountant doc

--- contracts/contracts/protocol/v1/FeesAndChargesAccountant.sol
+++ contracts/contracts/protocol/v1/FeesAndChargesAccountant.sol
@@ -2,6 +2,12 @@
 // SPDX-FileCopyrightText: Copyright 2024 Panther Ventures Limited Gibraltar
 pragma solidity ^0.8.19;
 
+/**
+ * @title Account
+ * @author Pantherprotocol Contributors
+ * @dev It calculates and accounts for fees associated with the panther protocol itself
+ * and its providers KYC/KYT providers, miners, and paymaster
+ */
 // solhint-disable-next-line no-empty-blocks
 contract FeesAndChargesAccountant {
 
