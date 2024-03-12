2ea951c0ee6e9d5f7cab6369b02866720d3accad
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-03-12 06:42:55 +0200

    feat(dapp): support taxi tree
    - Add support for taxi tree fast transactions.
    - Update the zero knowledge proof generation logic
      to accept both taxi and bus tree merkle proof.
    - Save and reuse transaction information for the
      taxi tree.
    - Generate taxi tree merkle proof.
    
    Issue-URL: https://app.clickup.com/t/8693zgup0

--- crypto/src/other/sparse-merkle-tree/constants.ts
+++ crypto/src/other/sparse-merkle-tree/constants.ts
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
 
-export const BUS_TREE_ZERO_LEAF =
+/**
+ * Leaf zero value (`keccak256("Pantherprotocol") % FIELD_SIZE`)
+ */
+export const TREE_ZERO_LEAF =
     '0x0667764c376602b72ef22218e1673c2cc8546201f9a77807570b3e5de137680d';
 export const BUS_TREE_EMPTY_BATCH =
     '0x2e99dc37b0a4f107b20278c26562b55df197e0b3eb237ec672f4cf729d159b69';
 export const BUS_TREE_BRANCH_DEPTH = 10;
 export const BUS_TREE_BATCH_DEPTH = 6;
 export const BUS_TREE_DEPTH = 26;
-
 export const FOREST_TREE_DEPTH = 2;
+export const TAXI_TREE_DEPTH = 2;
