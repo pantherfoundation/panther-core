c2bf611bd1c512a5f4176ec485a6fd841c75ba09
Bitnician <bitnician@gmail.com>
2024-04-16 15:19:13 +0330

    chore(contracts): deploy tokens behind proxy
    This patch deploys testnet tokens behind the proxy to give the deployers
    more control over how things work, especially if they need to make
    changes later.
    
    Issue-URL: https://app.clickup.com/t/8694amrcv

--- contracts/deploy/00-dev-dependecy/01b-pzkp-token-proxy-and-imp.ts
+++ contracts/deploy/00-dev-dependecy/01b-pzkp-token-proxy-and-imp.ts
@@ -7,25 +7,30 @@ import {DeployFunction} from 'hardhat-deploy/types';
 import {
     abi,
     bytecode,
-} from '../../deployments/ARCHIVE/externalAbis/ZKPToken.json';
+} from '../../deployments/ARCHIVE/externalAbis/PZkpToken.json';
 import {isProd} from '../../lib/checkNetwork';
 import {getNamedAccount} from '../../lib/deploymentHelpers';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     if (isProd(hre)) return;
     const deployer = await getNamedAccount(hre, 'deployer');
+    const multisig = await getNamedAccount(hre, 'multisig');
 
     const {
         deployments: {deploy},
     } = hre;
 
-    await deploy('Zkp_token', {
+    await deploy('PZkp_token', {
         contract: {
             abi,
             bytecode,
         },
+        proxy: {
+            proxyContract: 'EIP173Proxy',
+            owner: multisig,
+        },
         from: deployer,
-        args: [deployer],
+        args: [multisig],
         log: true,
         autoMine: true,
     });
@@ -33,4 +38,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
 export default func;
 
-func.tags = ['zkp-token', 'protocol-token', 'dev-dependency'];
+func.tags = ['pzkp-token', 'protocol-token', 'dev-dependency'];
