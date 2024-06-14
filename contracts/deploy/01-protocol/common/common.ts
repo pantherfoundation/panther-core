0425ee39461710ddc22b70f15599f331b713f443
Bogdan S <bogdan@pantherprotocol.io>
2024-06-14 11:54:30 +0300

    deploy(contracts): fetch token decimals
    Add function fetchDecimals and getTokenNam
    to not hardcode them and skip errors in scripts

---
+++ contracts/deploy/01-protocol/common/common.ts
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+
+import {BigNumber} from 'ethers';
+
+export const MaxUint128 = BigNumber.from(2).pow(128).sub(1);
+
+export enum FeeAmount {
+    LOW = 500,
+    MEDIUM = 3000,
+    HIGH = 10000,
+}
+
+export async function fetchDecimals(
+    tokenAddress: stringp,
+    provider: ethers.providers.Provider,
+) {
+    const tokenContract = new ethers.Contract(
+        tokenAddress,
+        ERC20_ABI,
+        provider,
+    );
+    const decimals = await tokenContract.decimals();
+    return decimals;
+}
+
+export async function getTokenName(
+    tokenAddress: string,
+    provider: ethers.providers.Provider,
+): Promise<string> {
+    const tokenContract = new ethers.Contract(
+        tokenAddress,
+        ERC20_ABI,
+        provider,
+    );
+    return await tokenContract.name();
+}
+
+export const ERC20_ABI = [
+    'function decimals() view returns (uint8)',
+    'function name() view returns (string)',
+];
