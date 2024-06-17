60da3cd6da0a47f294926e58920dd33109b85500
Bitnician <bitnician@gmail.com>
2024-06-17 16:13:06 +0330

    chore(contracts): add router deployment script

---
+++ contracts/deploy/01-protocol/46-uniswap-router-plugin.ts
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+import {getContractAddress, getNamedAccount} from '../../lib/deploymentHelpers';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const deployer = await getNamedAccount(hre, 'deployer');
+    const multisig = await getNamedAccount(hre, 'multisig');
+    const swapRouter = await getNamedAccount(hre, 'swapRouter');
+    const quoterV2 = await getNamedAccount(hre, 'quoterV2');
+
+    const {
+        deployments: {deploy},
+    } = hre;
+
+    const vaultProxyAddress = await getContractAddress(
+        hre,
+        'Vault_Proxy',
+        'VAULT_PROXY',
+    );
+
+    await deploy('UniswapV3RouterPlugin', {
+        from: deployer,
+        args: [swapRouter, quoterV2, vaultProxyAddress],
+        proxy: {
+            proxyContract: 'EIP173Proxy',
+            owner: multisig,
+        },
+        log: true,
+        autoMine: true,
+    });
+};
+export default func;
+
+func.tags = ['uni-router-plugin'];
+func.dependencies = ['check-params', 'deployment-consent'];
