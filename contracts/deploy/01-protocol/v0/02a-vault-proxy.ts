268034bbbe041db35f1561f888400f671e8534e2
akshayacode <akshaya071999@gmail.com>
2024-07-03 15:18:12 +0530

    deploy(contracts): add V0 deployment scripts
    This patch adds the deployment script for protocol V0
    
    Issue-URL: https://app.clickup.com/t/8694g4thy

---
+++ contracts/deploy/01-protocol/v0/02a-vault-proxy.ts
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+import {
+    reuseEnvAddress,
+    verifyUserConsentOnProd,
+} from '../../../lib/deploymentHelpers';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {
+        deployments: {deploy},
+        ethers,
+        getNamedAccounts,
+    } = hre;
+    const {deployer} = await getNamedAccounts();
+    await verifyUserConsentOnProd(hre, deployer);
+    if (reuseEnvAddress(hre, 'VAULT_PROXY')) return;
+
+    await deploy('Vault_Proxy', {
+        contract: 'EIP173Proxy',
+        from: deployer,
+        args: [
+            ethers.constants.AddressZero, // implementation will be changed
+            deployer, // owner will be changed
+            [], // data
+        ],
+        log: true,
+        autoMine: true,
+    });
+};
+export default func;
+
+func.tags = ['vault-proxy', 'protocol-v0'];
+func.dependencies = ['check-params'];
---
+++ contracts/deploy/01-protocol/v0/06-vault-proxy-owner-change.ts
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BUSL-1.1
+// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    const {getNamedAccounts, deployments, ethers} = hre;
+    const {deployer} = await getNamedAccounts();
+    const multisig =
+        process.env.DAO_MULTISIG_ADDRESS ||
+        (await getNamedAccounts()).multisig ||
+        deployer;
+
+    await deployments.fixture['Vault_Proxy'];
+    const deployment = await deployments.get('Vault_Proxy');
+
+    const vaultProxy = await ethers.getContractAt(
+        deployment.abi,
+        deployment.address,
+    );
+
+    const oldOwner = await vaultProxy.owner();
+    if (oldOwner.toLowerCase() == multisig.toLowerCase()) {
+        console.log(`Vault_Proxy owner is already set to: ${multisig}`);
+    } else {
+        console.log(`Transferring ownership of Vault_Proxy to ${multisig}...`);
+
+        const signer = await ethers.getSigner(deployer);
+        const tx = await vaultProxy.connect(signer).transferOwnership(multisig);
+
+        console.log('Vault_Proxy owner is updated, tx: ', tx.hash);
+    }
+};
+
+export default func;
+
+func.tags = ['vault-owner', 'pchain', 'protocol-v0'];
+func.dependencies = ['check-params', 'vault-upgrade'];
