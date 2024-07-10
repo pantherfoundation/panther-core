6140d898ad9b1a8c532dc6b2a669e787e73e37b4
Ahmed Ibrahim <me@ahmedibrahim.dev>
2024-07-10 08:34:11 +0300

    chore(dapp): update copyright for all file
    This update is applied using the copyright script.

--- crypto/src/other/triad-merkle-tree/constants.ts
+++ crypto/src/other/triad-merkle-tree/constants.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+// SPDX-FileCopyrightText: Copyright 2021-24 Panther Ventures Limited Gibraltar
 
 export const LEAF_NODE_SIZE = 3;
 export const TREE_DEPTH = 15;
--- crypto/src/utils/constants.ts
+++ crypto/src/utils/constants.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-// SPDX-FileCopyrightText: Copyright 2021-23 Panther Ventures Limited Gibraltar
+// SPDX-FileCopyrightText: Copyright 2021-24 Panther Ventures Limited Gibraltar
 
 // ALT_BN128 (also known as BN254) order and BabyJubJub field prime
 export const SNARK_FIELD_SIZE = BigInt(
