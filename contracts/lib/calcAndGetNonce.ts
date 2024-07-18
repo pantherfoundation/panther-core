59f4eec92b21b8a84a8e76e0f916ec9a0366ba20
Bogdan S <bogdan@pantherprotocol.io>
2024-07-18 14:19:22 +0300

    feat(contracts):  adopt calculate nonce
    This changes callculateAndGetNonce function to use not included to
    project EntryPoint contract for calculating Account address offchain
    
    Issue-URL: https://app.clickup.com/t/8694zdj8h

--- contracts/lib/calcAndGetNonce.ts
+++ contracts/lib/calcAndGetNonce.ts
@@ -11,18 +11,22 @@
  * @returns {Promise<any>} - The nonce retrieved from the EntryPoint contract.
  */
 
-import {EntryPoint} from '../../types/contracts';
+import {ethers} from 'hardhat';
+
+import {abi} from '../external/abi/EntryPoint.json';
 
 export async function callculateAndGetNonce(
     callData,
     walletAddress: string,
-    entryPoint: EntryPoint,
+    entryPointAddress: string,
 ) {
     // Hash the provided callData using keccak256
     const hashedData = ethers.utils.keccak256(callData);
 
     // Extract the lowest 11 bytes of the hashed data to use as a nonce
     const lowestBytesAsNonce = '0x' + hashedData.slice(-22);
+
+    const entryPoint = await ethers.getContractAt(abi, entryPointAddress);
 
     // Call the getNonce function of the EntryPoint contract with the walletAddress and calculated nonce
     return await entryPoint.getNonce(
