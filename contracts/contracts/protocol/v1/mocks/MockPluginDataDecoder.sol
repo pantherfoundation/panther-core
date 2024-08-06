404e54f2eccc2c2d232876c62e41a9d17b0cf219
akshayacode <akshaya071999@gmail.com>
2024-08-06 12:41:20 +0530

    test(contracts): add unit test for PluginDecoder
    This patch adds mock contract and unit test for PluginDataDecoder
    
    Issue-URL: https://app.clickup.com/t/86959m982

---
+++ contracts/contracts/protocol/v1/mocks/MockPluginDataDecoder.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright 2024 Panther Ventures Limited Gibraltar
+pragma solidity ^0.8.19;
+
+import "../plugins/PluginDataDecoderLib.sol";
+
+contract MockPluginDataDecoder {
+    using PluginDataDecoderLib for bytes;
+
+    function testExtractPluginAddress(
+        bytes memory data
+    ) external pure returns (address) {
+        return data.extractPluginAddress();
+    }
+
+    function testDecodeUniswapV3RouterExactInputSingleData(
+        bytes memory data
+    )
+        external
+        pure
+        returns (
+            uint32 deadline,
+            uint96 amountOutMinimum,
+            uint24 fee,
+            uint160 sqrtPriceLimitX96
+        )
+    {
+        return data.decodeUniswapV3RouterExactInputSingleData();
+    }
+
+    function testDecodeUniswapV3RouterExactInputData(
+        bytes memory data
+    )
+        external
+        pure
+        returns (uint32 deadline, uint96 amountOutMinimum, bytes memory path)
+    {
+        return data.decodeUniswapV3RouterExactInputData();
+    }
+
+    function testDecodeUniswapV3PoolData(
+        bytes memory data
+    ) external pure returns (address poolAddress, uint160 sqrtPriceLimitX96) {
+        return data.decodeUniswapV3PoolData();
+    }
+
+    function testDecodeQuickswapRouterExactInputSingleData(
+        bytes memory data
+    ) external pure returns (uint96 amountOutMin, uint32 deadline) {
+        return data.decodeQuickswapRouterExactInputSingleData();
+    }
+
+    function testDecodeQuickswapRouterExactInputData(
+        bytes memory data
+    )
+        external
+        pure
+        returns (uint96 amountOutMin, uint32 deadline, address[] memory path)
+    {
+        return data.decodeQuickswapRouterExactInputData();
+    }
+}
