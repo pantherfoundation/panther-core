f82a95c9f344c9fa6d4cbc76d0a881f96ee6dbea
Bitnician <bitnician@gmail.com>
2024-09-23 11:57:16 +0330

    refactor(contracts): static sub tree addresses
    This patch removes the static sub tree addresses.
    The Diamond tree proxy contract delegates call the facets (sub trees),
    so whenever the facet calls  another facet, the sender will be the
    panther trees proxy. As result, in the static tree, the panther trees
    proxy (aka `SELF`) must be authorized.

--- contracts/contracts/protocol/v1/trees/facets/StaticTree.sol
+++ contracts/contracts/protocol/v1/trees/facets/StaticTree.sol
@@ -24,16 +24,9 @@ import "../../../../common/crypto/PoseidonHashers.sol";
 // It's supposed to run on the mainnet only.
 // Bridges keepers are expected to propagate its root to other networks
 contract StaticTree is AppStorage, Ownable, IStaticTreeRootUpdater {
-    bytes32[50] private _gap;
-
     uint256 private constant NUM_LEAFS = 5;
 
-    address public immutable SELF;
-    address public immutable ZASSETS_TREE_CONTROLLER;
-    address public immutable BLACKLISTED_ZACCOUNTS_TREE_CONTROLLER;
-    address public immutable ZNETWORKS_TREE_CONTROLLER;
-    address public immutable ZZONES_TREE_CONTROLLER;
-    address public immutable PROVIDERS_KEYS_TREE_CONTROLLER;
+    address private immutable SELF;
 
     bytes32 private _staticTreeRoot;
     bytes32[NUM_LEAFS] public leafs;
@@ -47,30 +40,10 @@ contract StaticTree is AppStorage, Ownable, IStaticTreeRootUpdater {
         bytes32 updatedRoot
     );
 
-    constructor(
-        address _self,
-        address _zAssetsTreeController,
-        address _zAccountsBlacklistedTreeController,
-        address _zNetworksTreeController,
-        address _zZnonesTreeController,
-        address _providersKeysTreeController
-    ) {
-        require(
-            _self != address(0) &&
-                _zAssetsTreeController != address(0) &&
-                _zAccountsBlacklistedTreeController != address(0) &&
-                _zNetworksTreeController != address(0) &&
-                _zZnonesTreeController != address(0) &&
-                _providersKeysTreeController != address(0),
-            "init: zero address"
-        );
+    constructor(address _self) {
+        require(_self != address(0), "init: zero address");
 
         SELF = _self;
-        ZASSETS_TREE_CONTROLLER = _zAssetsTreeController;
-        BLACKLISTED_ZACCOUNTS_TREE_CONTROLLER = _zAccountsBlacklistedTreeController;
-        ZNETWORKS_TREE_CONTROLLER = _zNetworksTreeController;
-        ZZONES_TREE_CONTROLLER = _zZnonesTreeController;
-        PROVIDERS_KEYS_TREE_CONTROLLER = _providersKeysTreeController;
     }
 
     function initializeStaticTree() external onlyOwner {
@@ -97,32 +70,12 @@ contract StaticTree is AppStorage, Ownable, IStaticTreeRootUpdater {
     }
 
     function updateStaticRoot(bytes32 updatedLeaf, uint256 leafIndex) external {
-        require(msg.sender == _getLeafController(leafIndex), "unauthorized");
+        require(msg.sender == SELF, "unauthorized");
 
         leafs[leafIndex] = updatedLeaf;
         _staticTreeRoot = hash(leafs);
 
         emit RootUpdated(leafIndex, updatedLeaf, _staticTreeRoot);
-    }
-
-    function _getLeafController(
-        uint256 leafIndex
-    ) internal view returns (address leafController) {
-        require(leafIndex < NUM_LEAFS, "PF: INVALID_LEAF_IND");
-        if (leafIndex == ZASSET_STATIC_LEAF_INDEX)
-            leafController = ZASSETS_TREE_CONTROLLER;
-
-        if (leafIndex == BLACKLISTED_ZACCOUNT_STATIC_LEAF_INDEX)
-            leafController = BLACKLISTED_ZACCOUNTS_TREE_CONTROLLER;
-
-        if (leafIndex == ZNETWORK_STATIC_LEAF_INDEX)
-            leafController = ZNETWORKS_TREE_CONTROLLER;
-
-        if (leafIndex == ZZONE_STATIC_LEAF_INDEX)
-            leafController = ZZONES_TREE_CONTROLLER;
-
-        if (leafIndex == PROVIDERS_KEYS_STATIC_LEAF_INDEX)
-            leafController = PROVIDERS_KEYS_TREE_CONTROLLER;
     }
 
     function hash(bytes32[5] memory input) private pure returns (bytes32) {
