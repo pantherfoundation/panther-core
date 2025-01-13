b8c457f0fbc3f9c70830fe24550102bb74557225
Sushma Varadaiah <sushma@pantherprotocol.io>
2025-01-13 18:14:08 +0400

    fix(circuits): v2 data escrow implementation testing
    With these we are testing version 2 of data escrow implementation.

--- circuits/test/ZZoneDataEscrowElGamalEncryptionPoint.test.ts
+++ circuits/test/ZZoneDataEscrowElGamalEncryptionPoint.test.ts
@@ -5,6 +5,7 @@ const wasm_tester = cicom_wasm_tester.wasm;
 
 import {getOptions} from './helpers/circomTester';
 import {babyjub} from 'circomlibjs';
+import poseidon from 'circomlibjs/src/poseidon';
 
 describe('ZZone Data Escrow ElGamal Encryption', function (this: any) {
     let zZoneDataEscrowElGamalEncryption: any;
@@ -19,33 +20,101 @@ describe('ZZone Data Escrow ElGamal Encryption', function (this: any) {
         zZoneDataEscrowElGamalEncryption = await wasm_tester(input, opts);
     });
 
+    const SNARK_FIELD =
+        21888242871839275222246405745257275088548364400416034343698204186575808495617n;
+
+    // EphemeralPubKeysBuilder
+    // Input: pubKey[2], ephemeralRandom
+    // Output: ephemeralPubKey[2], sharedPubKey[2]
     const zZoneDataEscrowEphemeralRandom =
-        790122152066676684676093302872898287841903882354339429497975929636832086290n;
+        790122152066676684676093302872898287841903882354339429497975929636832086290n; // zZoneDataEscrowEphemeralRandom
 
     const zZoneEdDsaPubKey = [
         13969057660566717294144404716327056489877917779406382026042873403164748884885n,
         11069452135192839850369824221357904553346382352990372044246668947825855305207n,
-    ];
+    ]; // zZoneEdDsaPubKey
 
+    // Input to DataEscrowElGamalEncryptionPoint
     const pointMessage = [
         [
-            4301916310975298895721162797900971043392040643140207582177965168853046592976n,
-            815388028464849479935447593762613752978886104243152067307597626016673798528n,
+            4301916310975298895721162797900971043392040643140207582177965168853046592976n, // dataEscrowEphemeralPubKeyAx
+            815388028464849479935447593762613752978886104243152067307597626016673798528n, // dataEscrowEphemeralPubKeyAy
         ],
     ];
 
-    const sharedPubKey0 = [
-        [
-            3579452621007862409166467823704846427808645445097270817162531245395116740794n,
-            21104711759309908609089048597965635688447821050509879948755813223129622499159n,
-        ],
-    ];
-
-    const encryptedMessage0 = babyjub.addPoint(
-        pointMessage[0],
-        sharedPubKey0[0],
+    const sharedPubKey = babyjub.mulPointEscalar(
+        zZoneEdDsaPubKey,
+        zZoneDataEscrowEphemeralRandom.toString(),
     );
-    // console.log('encryptedMessage0=>', encryptedMessage0);
+
+    const ephemeralPubKey = babyjub.mulPointEscalar(
+        babyjub.Base8,
+        zZoneDataEscrowEphemeralRandom,
+    );
+    // ephemeralPubKey=> [
+    //     8203289148254703516772267706874329469330087297928457772489392227653451244213n, - zZoneDataEscrowEphemeralPubKeyAx
+    //     19998992060707539017877331634603765261877243592349009808298088607668947098216n - zZoneDataEscrowEphemeralPubKeyAy
+    //   ]
+    // console.log('ephemeralPubKey=>', ephemeralPubKey);
+
+    // DataEscrowElGamalEncryptionPoint(PointsSize) computations
+    // [1] - create k-seed
+    const kSeed = poseidon([sharedPubKey[0], sharedPubKey[1]]);
+
+    // [2] - encrypted data
+    const helperHash = poseidon([kSeed, 0]);
+    const encryptedMessage = pointMessage[0][0] + helperHash;
+    // 21022076763366182175477357918933442131485472229146225036655452082762082124322n
+    // console.log('encryptedMessage=>', encryptedMessage);
+
+    const moduloEncryptedMessage = encryptedMessage % SNARK_FIELD;
+    // 21022076763366182175477357918933442131485472229146225036655452082762082124322n
+    // console.log('moduloEncryptedMessage=>', moduloEncryptedMessage);
+
+    // [3] - cipher message hash
+    const encryptedMessageHash = poseidon([encryptedMessage]);
+    // encryptedMessageHash=> 12444185887568679379186315345524877022401565154030591259044587421593238202125n
+    // console.log('encryptedMessageHash=>', encryptedMessageHash);
+
+    // [4] - hmac
+    const kMac = poseidon([kSeed, 1]);
+
+    // 0xd836363636363636363636363636363636363636363636363636363636363636
+    let ipad =
+        97795359191332584535587663717355991292619291276890730051339681934624425391670n;
+
+    let ipadModular = ipad % SNARK_FIELD;
+
+    function bigIntXOR(a: bigint, b: bigint) {
+        let abMul2 = (2n * a * b) % SNARK_FIELD;
+        let aplusb = a + b;
+        let aplusBMinusAbMul2 = aplusb - abMul2;
+        let resultOfXor =
+            ((aplusBMinusAbMul2 % SNARK_FIELD) + SNARK_FIELD) % SNARK_FIELD;
+        return resultOfXor;
+    }
+
+    const kMacInner = bigIntXOR(kMac, ipadModular);
+    // 21797284874701574539809215683527628528498093308347464104746414623680031777585n
+    // console.log('kMacInner=>', kMacInner);
+
+    let innerHMacSize = 2; // 1+1
+    const innerHMacHash = poseidon([kMacInner, moduloEncryptedMessage]);
+    // 19457888664929498929543237240690469023928612450344569641056481447681365892438n
+    // console.log('innerHMacHash=>', innerHMacHash);
+
+    // outerXor computation
+    // 0x1c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c5c;
+    let opad =
+        12827947140996794198885805201942876066097557124722925231822261757739395734620n;
+    let opadModular = opad % SNARK_FIELD;
+    // console.log("opadModular=>",opadModular);
+
+    const kMacOuter = bigIntXOR(kMac, opadModular);
+
+    const hmacHash = poseidon([kMacOuter, innerHMacHash]); // hmacSize = 2
+    // 11881728848936312288293767908225444888306857512351728722791695602487731265197n
+    // console.log('hmacHash=>', hmacHash);
 
     const input = {
         ephemeralRandom: zZoneDataEscrowEphemeralRandom,
@@ -54,17 +123,12 @@ describe('ZZone Data Escrow ElGamal Encryption', function (this: any) {
     };
 
     const output = {
-        ephemeralPubKey: [
-            8203289148254703516772267706874329469330087297928457772489392227653451244213n,
-            19998992060707539017877331634603765261877243592349009808298088607668947098216n,
-        ],
-        encryptedMessage: [
-            [
-                2468338014121331792444587249698692818014762050360212301429643474146750905863n,
-                16996420608431036321409292371827871642706083345802290766905570404093710249010n,
-            ],
-        ],
+        ephemeralPubKey: [ephemeralPubKey[0], ephemeralPubKey[1]], // 8203289148254703516772267706874329469330087297928457772489392227653451244213n, 19998992060707539017877331634603765261877243592349009808298088607668947098216n
+        encryptedMessage: [encryptedMessage], // 21022076763366182175477357918933442131485472229146225036655452082762082124322n
+        encryptedMessageHash: encryptedMessageHash, // 12444185887568679379186315345524877022401565154030591259044587421593238202125n
+        hmac: hmacHash, // 11881728848936312288293767908225444888306857512351728722791695602487731265197n
     };
+
     describe('Valid input signals', function () {
         it('should compute valid witness for non zero input tx', async () => {
             const wtns =
@@ -79,19 +143,21 @@ describe('ZZone Data Escrow ElGamal Encryption', function (this: any) {
             const indexOfephemeralPubKey1 = wtns.indexOf(
                 output.ephemeralPubKey[1],
             );
-            const indexOfencryptedMessage00 = wtns.indexOf(
-                output.encryptedMessage[0][0],
+            const indexOfencryptedMessage = wtns.indexOf(
+                output.encryptedMessage[0],
             );
-            const indexOfencryptedMessage01 = wtns.indexOf(
-                output.encryptedMessage[0][1],
+            const indexOfencryptedMessageHash = wtns.indexOf(
+                output.encryptedMessageHash,
             );
+            const indexOfhmac = wtns.indexOf(output.hmac);
 
             const wtnsFormattedOutput = [
                 0,
                 wtns[indexOfephemeralPubKey0],
                 wtns[indexOfephemeralPubKey1],
-                wtns[indexOfencryptedMessage00],
-                wtns[indexOfencryptedMessage01],
+                wtns[indexOfencryptedMessage],
+                wtns[indexOfencryptedMessageHash],
+                wtns[indexOfhmac],
             ];
 
             await zZoneDataEscrowElGamalEncryption.assertOut(
--- circuits/test/ZZoneDataEscrowEphemeralPubKeysBuilder.test.ts
+++ circuits/test/ZZoneDataEscrowEphemeralPubKeysBuilder.test.ts
@@ -28,40 +28,17 @@ describe('ZZone DataEscrow - EphemeralPubKeysBuilder circuit', function (this: a
         11069452135192839850369824221357904553346382352990372044246668947825855305207n,
     ];
 
-    // ephemeralRandom0 is the given input ephemeralRandom - 0
-
-    // sharedPubKey0 - [0,0] & [0,1]
-    const sharedPubKey0 = babyjub.mulPointEscalar(
+    const sharedPubKey = babyjub.mulPointEscalar(
         pubKey,
         ephemeralRandom.toString(),
     );
-    // console.log('sharedPubKey0=>', sharedPubKey0);
+    // console.log('sharedPubKey=>', sharedPubKey);
 
-    // ephemeralPubKey0 - [0,0] & [0,1]
-    const ephemeralPubKey0 = babyjub.mulPointEscalar(
+    const ephemeralPubKey = babyjub.mulPointEscalar(
         babyjub.Base8,
         ephemeralRandom,
     );
-    // console.log('ephemeralPubKey0=>', ephemeralPubKey0);
-
-    // hidden point computation
-    const hiddenPoint_poseidon1 = poseidon([
-        sharedPubKey0[0],
-        sharedPubKey0[1],
-    ]);
-    const hiddenPoint_poseidon = poseidon([hiddenPoint_poseidon1]);
-    let mask = (BigInt(1) << BigInt(252)) - BigInt(1);
-    let hiddenPoint252Bits = BigInt(hiddenPoint_poseidon) & mask;
-
-    const hiddenPoint_eMult = babyjub.mulPointEscalar(
-        pubKey,
-        hiddenPoint252Bits,
-    );
-    // hiddenPoint_eMult=> [
-    //     3285643778013373851751435147704859609182820956390679408406396837095210775191n,
-    //     19488993062119338329039070493533050536743750381173930389742335045938707033749n
-    //   ]
-    // console.log('hiddenPoint_eMult=>', hiddenPoint_eMult);
+    // console.log('ephemeralPubKey=>', ephemeralPubKey);
 
     const input = {
         pubKey: [
@@ -73,22 +50,8 @@ describe('ZZone DataEscrow - EphemeralPubKeysBuilder circuit', function (this: a
     };
 
     const output = {
-        ephemeralPubKey: [
-            [
-                8203289148254703516772267706874329469330087297928457772489392227653451244213n,
-                19998992060707539017877331634603765261877243592349009808298088607668947098216n,
-            ],
-        ],
-        sharedPubKey: [
-            [
-                3579452621007862409166467823704846427808645445097270817162531245395116740794n,
-                21104711759309908609089048597965635688447821050509879948755813223129622499159n,
-            ],
-        ],
-        hidingPoint: [
-            3285643778013373851751435147704859609182820956390679408406396837095210775191n,
-            19488993062119338329039070493533050536743750381173930389742335045938707033749n,
-        ],
+        ephemeralPubKey: [ephemeralPubKey[0], ephemeralPubKey[1]],
+        sharedPubKey: [sharedPubKey[0], sharedPubKey[1]],
     };
 
     describe('Valid input signals', function () {
